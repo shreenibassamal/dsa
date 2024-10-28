4d93e77cb8d8431859f6bbcf007ebbc75350fcba
Shreenibas Samal <shreenibassamal@gmail.com>
2024-10-28 10:21:58 +0530

    printing number 1 to 5

---
+++ numbers/1.2 PrintNumber1to5.java
@@ -0,0 +1,10 @@
+package numbers;
+
+//print 1 to 5 using for loop
+class PrintNumber1to5{
+	public static void main(String[]args){
+		for(int a = 1;a<=5;a++){
+			System.out.println(a);
+		}
+	}
+}
